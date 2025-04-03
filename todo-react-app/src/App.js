8891bac793ab8af84a1124a9eea05a099fa52fb6
YO1231 <whee0211@gmail.com>
2025-04-03 10:16:18 +0900

    웹프로그래밍응용 수업 0403 실습

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import Todo from './Todo';
+import React, {useState} from 'react';
 
 function App() {
-  let name = "YKH";
-  
-  let output = <div className="App">{name}
-    <Todo />
-    <Todo />
-    <Todo />
-  </div>;
+  const [items, setItems] = useState([
+    {
+    id: "0",
+    title: "Hello World 1",
+    done: true 
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true 
+      }
+  ]);
+
+  let todoItems = 
+    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
 
     return (
-      output
+      <div className="App">
+        {todoItems}
+      </div>
     );  
 }
 
